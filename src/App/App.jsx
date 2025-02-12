873c2b78f213ee49e37fac5ee0c36ddd73898556
Maria Khmel <mariakhmel8@gmail.com>
2025-02-13 00:11:17 +0200

    header

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,19 @@
+import SearchBar from "../SearchBar/SearchBar";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
-     
+      <div>
+        <Toaster />
+      </div>
+      <SearchBar onSubmit={onSubmit} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -1,8 +1,27 @@
-const SearchBar = () => {
+import toast from "react-hot-toast";
+import css from "./SearchBar.module.css";
+
+const SearchBar = ({ onSubmit }) => {
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    if (e.target.elements.input.value === "") {
+      toast.error("Please enter the searchword");
+    }
+  };
+
   return (
-    <>
-      <div>SearchBar</div>
-    </>
+    <header className={css.searchBar}>
+      <form onSubmit={handleSumbit}>
+        <input
+          name="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
   );
 };
 
